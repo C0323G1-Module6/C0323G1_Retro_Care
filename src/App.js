f2e7effccbf7a3d70c4a4710aaad4019ae07846c
mrhuyle <mrhuyle@gmail.com>
2023-09-18 10:26:14 +0700

    [HUYL][home] add readme, Dashboard, Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
     </Routes>
   );
 }
