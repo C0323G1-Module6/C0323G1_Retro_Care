239d43969f48b737efdb809e280fc3b1bdc4bde9
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-09-19 01:47:56 +0700

    [User][NhatNHH] create login by customer, facebook, register

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrescriptionList from "./components/prescription/PrescriptionList";
+import Login from "./components/user/Login";
+import Register from "./components/user/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />}></Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />}/>
       <Route path="/dashboard" element={<Dashboard />}>
         <Route
           path="/dashboard/prescription"
