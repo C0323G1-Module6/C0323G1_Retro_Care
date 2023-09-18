4d2d57b3397e29cba04e62f78ee9b984351c6010
mrhuyle <mrhuyle@gmail.com>
2023-09-18 21:27:07 +0700

    [HUYL][home][dashboard]

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Dashboard from "./components/dashboard/Dashboard";
+import PrescriptionList from "./components/presciption/PrescriptionList";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route
+          path="/dashboard/presciption"
+          element={<PrescriptionList />}
+        ></Route>
+      </Route>
     </Routes>
   );
 }
