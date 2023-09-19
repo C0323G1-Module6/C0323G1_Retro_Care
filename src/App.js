9c4a2c33c480fa2019bfb419611460eb6bee3146
mrhuyle <mrhuyle@gmail.com>
2023-09-19 17:03:49 +0700

    [HUYL][home][edit app.js]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrescriptionList from "./components/prescription/PrescriptionList";
@@ -9,25 +9,30 @@ import KindOfMedicineList from "./components/kindOfMedicine/KindOfMedicineList";
 import CustomerList from "./components/customer/CustomerList";
 import ListEmployee from "./components/employee/ListEmployee";
 import MedicineList from "./components/medicine/MedicineList";
+import PrescriptionCreate from "./components/prescription/PrescriptionCreate";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/home" element={<Home/>}></Route>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}>
-                <Route path="/dashboard/prescription" element={<PrescriptionList/>}/>
-                <Route path="/dashboard/medicine" element={<MedicineList />}/>
-                <Route
-                    path="/dashboard/kind-of-medicine"
-                    element={<KindOfMedicineList/>}
-                />
-                <Route path="/dashboard/customer" element={<CustomerList/>}/>
-                <Route path="/dashboard/list-employee" element={<ListEmployee/>}/>
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="/dashboard/prescription" element={<PrescriptionList />} />
+        <Route
+          path="/dashboard/prescription/create"
+          element={<PrescriptionCreate />}
+        />
+        <Route path="/dashboard/medicine" element={<MedicineList />} />
+        <Route
+          path="/dashboard/kind-of-medicine"
+          element={<KindOfMedicineList />}
+        />
+        <Route path="/dashboard/customer" element={<CustomerList />} />
+        <Route path="/dashboard/list-employee" element={<ListEmployee />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
