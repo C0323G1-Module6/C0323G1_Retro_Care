d504debc821bd3c66c76deb3ce73825ec87bf2a2
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-09-19 16:28:29 +0700

    [HuyHD][invoice][list]

--- src/components/invoice/ServiceInvoice.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export function getMaxCode() {
-    try {
-        const result = axios.get("http://localhost:8080/api/invoice/code")
-    } catch (e) {
-        console.log(e);
-    }
-}
