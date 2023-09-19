241c0f4d75a9e8c54f9ea9200ba66879495c6c11
AnhDao19112001 <phantaanhdao@gmail.com>
2023-09-19 15:15:23 +0700

    [DaoPTA][Medicine list]

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrescriptionList from "./components/prescription/PrescriptionList";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 import KindOfMedicineList from "./components/kindOfMedicine/KindOfMedicineList";
+import MedicineList from "./components/medicine/MedicineList";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/home" element={<Home />}></Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />}/>
-      <Route path="/dashboard" element={<Dashboard />}>
-        <Route path="/dashboard/prescription" element={<PrescriptionList />} />
-        <Route
-          path="/dashboard/kind-of-medicine"
-          element={<KindOfMedicineList />}
-        />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/home" element={<Home/>}></Route>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}>
+                <Route path="/dashboard/medicine" element={<MedicineList/>}/>
+                <Route path="/dashboard/prescription" element={<PrescriptionList/>}/>
+                <Route
+                    path="/dashboard/kind-of-medicine"
+                    element={<KindOfMedicineList/>}
+                />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- src/services/medicine/MedicineService.js
+++ src/services/medicine/MedicineService.js
@@ -1,1 +1,19 @@
-// Dao va Tin Code trong nay
+import axios, {get} from "axios";
+
+export const findAll = async () => {
+    try{
+        const result = await axios.get(`http://localhost:8080/api/medicine/get-medicine`);
+        return result.data
+    }catch (error){
+        return error;
+    }
+}
+
+export const getListMedicine = async (page, size) => {
+    try {
+        const result =  await axios.get(`http://localhost:8080/api/medicine/get-medicine?page=${page}&size=${size}`);
+        return result.data
+    }catch (error){
+        return null;
+    }
+}
