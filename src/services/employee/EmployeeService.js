8aa45b3c76f8b6af894743c7d102f8520052675f
nvt2802 <nvt280202@gmail.com>
2023-09-19 17:08:43 +0700

    [tannv][employee] layout update create

--- src/services/employee/EmployeeService.js
+++ src/services/employee/EmployeeService.js
@@ -21,3 +21,25 @@ export async function searchListEmployee(page, limit, sort, idPosition, nameEmpl
 export async function deleteEmployee(idEmployee) {
     await axios.delete("/employees/delete-employee", {params: {id: idEmployee}})
 }
+
+export const getNewEmployee =async () =>{
+    const res = await axios.get('http://localhost:8080/employees/create');
+    return res
+}
+
+export const getEmployee =async (id) =>{
+    const res = await axios.get(`http://localhost:8080/employees/${id}`);
+    return res
+}
+export const updateEmployee =async (employee) =>{
+    const res = await axios.patch(`http://localhost:8080/employees/update/${employee.id}`,employee);
+    return res
+}
+export const crateEmployee =async (employeeDto) =>{
+    const res = await axios.post('http://localhost:8080/employees/create',employeeDto);
+    return res
+}
+export const createUserEmployee = async (userName) =>{
+    const res= await axios.post(`http://localhost:8080/api/user/register-by-manager?userName=${userName}`);
+    return res;
+}
