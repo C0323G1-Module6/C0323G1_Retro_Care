7d5ddb7ac1f9b759f47403f59012d798595f058a
mrhuyle <mrhuyle@gmail.com>
2023-09-19 10:26:43 +0700

    [HUYL][home][app.js customer]

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import PrescriptionList from "./components/prescription/PrescriptionList";
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 import KindOfMedicineList from "./components/kindOfMedicine/KindOfMedicineList";
+import CustomerList from "./components/customer/CustomerList";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />}></Route>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />}/>
+      <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />}>
         <Route path="/dashboard/prescription" element={<PrescriptionList />} />
         <Route
           path="/dashboard/kind-of-medicine"
           element={<KindOfMedicineList />}
         />
+        <Route path="/dashboard/customer" element={<CustomerList />} />
       </Route>
     </Routes>
   );
