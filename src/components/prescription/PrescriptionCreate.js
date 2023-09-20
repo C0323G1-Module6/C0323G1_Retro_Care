c0b01f52e652a42523bdc1800f37097c38a7ec4f
0961297922a <kimthanh130901@gmail.com>
2023-09-20 08:32:08 +0700

    [ThanhKN][prescription] update createPrescription

--- src/components/prescription/PrescriptionCreate.js
+++ src/components/prescription/PrescriptionCreate.js
@@ -1,44 +1,71 @@
 import { Field, FieldArray, Form, Formik } from "formik";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getAllPatient } from "../../services/prescription/patient";
+import { findAll } from "../../services/medicine/MedicineService";
+import { createPrescription } from "../../services/prescription/prescription";
+import { createIndication } from "../../services/prescription/indication";
 
+
+const initialValues = {
+    indication: [
+        {
+            medicine: "",
+            dosage: "",
+            frequency: "",
+        }
+    ]
+};
 function PrescriptionCreate() {
-    const [patients, setPatients] = useState([])
+    const [patients, setPatients] = useState([]);
+    const [chooseMedicines, setChooseMedicines] = useState([]);
+    const btnRef = useRef(null);
 
     const findAllPatient = async () => {
-        const res = await getAllPatient()
-        console.log(res);
+        const res = await getAllPatient();
         setPatients(res)
     };
 
-    const indicationValue = {
-        indication: [
-            {
-                dosage :"",
-                frequency : "",
+    const findAllMedicine = async () => {
+        const res = await findAll();
+        setChooseMedicines(res.content);
+    }
 
-            }
-        ]
-    };
+    const createNewPrescription = async (value) => {
+        await createPrescription(value);
+    }
+
+    const createNewIndication = async (value) => {
+        await createIndication(value);
+    }
+
+
+
+
     useEffect(() => {
         findAllPatient();
+        findAllMedicine();
     }, [])
 
     return (
         <>
-
             <div className="d-flex flex-wrap gap-3 justify-content-center mt-10">
-                <fieldset className="border border-dark rounded-3 p-3 w-50" style={{ backgroundColor: '#f8f9fa' }}>
-                    <Formik
+                <Formik
                     initialValues={{
-                        code:"",
-                        name:"",
-                        symptoms:"",
+                        code: "",
+                        name: "",
+                        symptoms: "",
                         patient: "",
-                        duratiton: "",
+                        duration: "",
                         note: ""
                     }}
-                    >
+
+                    onSubmit={(values) => {
+                        console.log(values);
+                        createNewPrescription(values);
+                    }}
+                >
+                    <fieldset className="border border-dark rounded-3 p-3 w-50" style={{ backgroundColor: '#f8f9fa' }}>
+
                         <Form>
                             <legend className="float-none w-auto px-3">Thông tin đơn thuốc</legend>
                             <div className="mb-3 row">
@@ -64,7 +91,7 @@ function PrescriptionCreate() {
                                 <div className="col-sm-4">
                                     <Field as='select' className="form-select" aria-label="Default select example" name='patient'>
                                         {
-                                            patients.map((t)=>(
+                                            patients.map((t) => (
                                                 <option value={`${JSON.stringify(t)}`}>{t.name}</option>
                                             ))
                                         }
@@ -72,7 +99,7 @@ function PrescriptionCreate() {
                                 </div>
                                 <label className="col-sm-3 col-form-label" id="label-input">Số ngày uống </label>
                                 <div className="col-sm-2">
-                                    <Field type="number" className="form-control" name='duration'/>
+                                    <Field type="number" className="form-control" name='duration' />
                                 </div>
                             </div>
                             <div className="mb-3 row">
@@ -81,62 +108,101 @@ function PrescriptionCreate() {
                                     <Field type="text" className="form-control" name='note' />
                                 </div>
                             </div>
+                            <button type="submit" ref={btnRef} >oke</button>
                         </Form >
-                    </Formik>
-                    <Formik>
-                        <Form>
-                            <FieldArray>
-                                <div className="d-flex flex-wrap gap-3 justify-content-center mt-10">
-                                    <fieldset className="border border-dark rounded-3 p-3 w-100">
-                                        <legend className="float-none w-auto px-3">Chỉ định</legend>
-                                        <div className="mb-3 row d-flex align-items-center justify-content-start">
-                                            <label className="col-sm-1 col-form-label">1.</label>
-                                            <div className="col-sm-4">
-                                                <select className="form-select" aria-label="Default select example">
-                                                    <option selected>Chọn thuốc</option>
-                                                    <option value={1}>Thuốc 1</option>
-                                                    <option value={2}>Thuốc 2</option>
-                                                    <option value={3}>Thuốc 3</option>
-                                                </select>
-                                            </div>
-                                            <label className="col-sm-3 col-form-label">Số viên:</label>
-                                            <div className="col-sm-2">
-                                                <input type="text" className="form-control" placeholder="..." />
-                                            </div>
-                                            <div className="col-sm-2">
-                                                <button type="button" className="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i className="fa-solid fa-trash" />
-                                                    Xoá
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div className="mb-3 row d-flex align-items-center justify-content-start">
-                                            <div className="col-sm-1">&nbsp;</div>
-                                            <label className="col-sm-2 col-form-label">Ngày uống: </label>
-                                            <div className="col-sm-2">
-                                                <input type="text" className="form-control" placeholder="..." />
-                                            </div>
-                                            <label className="col-sm-1 col-form-label">lần,</label>
-                                            <label className="col-sm-2 col-form-label">Mỗi lần: </label>
-                                            <div className="col-sm-2">
-                                                <input type="text" className="form-control" placeholder="..." />
-                                            </div>
-                                            <label className="col-sm-1 col-form-label">viên</label>
-                                        </div>
-                                        <div className="d-flex justify-content-left align-items-center">
-                                            <button className=" btn btn-outline-primary"><i className="fa-solid fa-plus" />Thêm mới thuốc</button>
-                                        </div>
-                                    </fieldset>
-                                    <div className="d-flex justify-content-end w-100 gap-2">
-                                        <a href="ThanhKN_ListPrescription.html" className=" btn btn-outline-primary"><i className="fa-solid fa-plus" />
-                                            Thêm mới</a>
-                                        <a href="ThanhKN_ListPrescription.html" className="btn btn-outline-primary"><i className="fa-regular fa-circle-left" />Trở về</a>
-                                    </div>
-                                </div>
-                            </FieldArray>
-                        </Form>
-                    </Formik >
-                </fieldset>
-            </div>
+
+
+
+                        <div className="d-flex flex-wrap gap-3 justify-content-center mt-10">
+                            <Formik
+                                initialValues={initialValues}
+                                onSubmit={(value)=>{
+                                    // console.log(value);
+                                }}
+                            >
+                                {({ values }) => (
+                                    <Form>
+                                        <FieldArray name="indication">
+                                            {({ remove, push }) => (
+                                                <fieldset className="border border-dark rounded-3 p-3 w-100">
+                                                    <legend className="float-none w-auto px-3">Chỉ định</legend>
+                                                    {values.indication.length > 0 && values.indication.map((i, index) => (
+                                                        <div>
+                                                            <div className="mb-3 row d-flex align-items-center justify-content-start">
+                                                                <label className="col-sm-1 col-form-label">{index + 1}.</label>
+                                                                <div className="col-sm-4">
+                                                                    <Field
+                                                                        type="text"
+                                                                        className="form-control"
+                                                                        placeholder="Tìm thuốc..."
+                                                                        id="search-input"
+                                                                        // value={inputMedicine}
+                                                                        // onChange={(event) => {
+                                                                        //     setInputMedicine(event.target.value);
+                                                                        // }}
+                                                                        name={`indication[${index}].medicine`}
+                                                                        list="medicine-options"
+                                                                    />
+
+                                                                    <datalist id="medicine-options">
+                                                                        {chooseMedicines.map((medicine, index) => (
+                                                                            <option key={medicine.id} value={medicine.name}></option>
+                                                                        ))}
+                                                                    </datalist>
+                                                                    
+                                                                </div>
+                                                                <label className="col-sm-3 col-form-label">Số viên:</label>
+                                                                <div className="col-sm-2">
+                                                                    <input type="text" className="form-control" placeholder="..." />
+                                                                </div>
+                                                                <div className="col-sm-2">
+                                                                    <button type="button" className="btn btn-outline-primary" onClick={() => remove(index)}><i className="fa-solid fa-trash" />
+                                                                        Xoá
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+
+                                                            <div className="mb-3 row d-flex align-items-center justify-content-start">
+                                                                <div className="col-sm-1">&nbsp;</div>
+                                                                <label className="col-sm-2 col-form-label">Ngày uống: </label>
+                                                                <div className="col-sm-2">
+                                                                    <Field type="text" className="form-control" name={`indication[${index}].frequency`} placeholder="..." />
+                                                                </div>
+                                                                <label className="col-sm-1 col-form-label">lần,</label>
+                                                                <label className="col-sm-2 col-form-label">Mỗi lần: </label>
+                                                                <div className="col-sm-2">
+                                                                    <Field type="text" className="form-control" name={`indication[${index}].dosage`} placeholder="..." />
+                                                                </div>
+                                                                <label className="col-sm-1 col-form-label">viên</label>
+                                                            </div>
+                                                        </div>
+                                                    ))}
+                                                    <div className="d-flex justify-content-left align-items-center">
+                                                        <button
+                                                            className="btn btn-outline-primary" 
+                                                            onClick={() => {
+                                                                push({ medicine: '', dosage: '', frequency: '' });
+                                                                
+                                                            }}
+                                                            
+                                                        ><i className="fa-solid fa-plus" />Thêm mới thuốc</button>
+                                                    </div>
+                                                </fieldset>
+
+                                            )}
+                                        </FieldArray>
+                                    </Form>
+                                )}
+                            </Formik >
+                            <div className="d-flex justify-content-end w-100 gap-2">
+                                <button onClick={()=> btnRef.current.click()}  className=" btn btn-outline-primary" ><i className="fa-solid fa-plus" />
+                                    Thêm mới</button>
+                                <a href="ThanhKN_ListPrescription.html" className="btn btn-outline-primary"><i className="fa-regular fa-circle-left" />Trở về</a>
+                            </div>
+                        </div>
+                    </fieldset>
+                </Formik>
+            </div >
         </>
     )
 }
