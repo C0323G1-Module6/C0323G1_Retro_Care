b447734eb19c5302c897186d25de7d17dc7f9e16
LuuMinhHanhNguyen <luuminhhanhnguyen@gmail.com>
2023-09-22 20:46:24 +0700

    [HanNLM]update fe 22923

--- src/services/medicine/MedicineService.js
+++ src/services/medicine/MedicineService.js
@@ -3,29 +3,43 @@ import axios from "axios";
 export const getAllUnit = async () => {
   const result = await axios.get("http://localhost:8080/api/unit");
   return result.data;
-};
+}
 
 export const addMedicine = async (medicine) => {
   await axios.post("http://localhost:8080/api/medicine", medicine);
-};
+}
 
 export const editMedicine = async (id, medicine) => {
-  await axios.patch(`http://localhost:8080/api/medicine/${id}`, medicine);
-};
+  try {
+    await axios.patch(`http://localhost:8080/api/medicine/${id}`, medicine);
+  } catch (error) {
+    console.log(error)
+  }
+
+}
 
 export const getMedicineById = async (id) => {
-  await axios.get(`http://localhost:8080/api/medicine/${id}`);
-};
+  try {
+    const result = await axios.get(`http://localhost:8080/api/medicine/${id}`);
+    return result.data;
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export async function getAllKindOfMedicine() {
   const res = await axios.get(`http://localhost:8080/api/kindOfMedicine`);
+  return res.data;
+}
+export async function getCountries() {
+  const res = await axios.get('https://restcountries.com/v3.1/all')
   return res.data;
 }
 
 export const findAll = async (page) => {
   try {
     const result = await axios.get(
-      `http://localhost:8080/api/medicine/get-medicine?page=${page}&size=${5}`
+        `http://localhost:8080/api/medicine/get-medicine?page=${page}&size=${5}`
     );
     return result;
   } catch (error) {
@@ -33,13 +47,14 @@ export const findAll = async (page) => {
   }
 };
 
+
 export const searchMedicine = async (searchInMedicine, search, page, limit, conditional) => {
   try {
     const result = await axios.get(
-      `http://localhost:8080/api/medicine/search?search=${search}&searchInMedicine=${searchInMedicine}&page=${page}&limit=${limit}&conditional=${conditional}`
+        `http://localhost:8080/api/medicine/search?search=${search}&searchInMedicine=${searchInMedicine}&page=${page}&limit=${limit}&conditional=${conditional}`
     );
     console.log(result.data)
-    return result.data;
+    return result;
   } catch (error) {
     return error;
   }
@@ -48,7 +63,7 @@ export const searchMedicine = async (searchInMedicine, search, page, limit, cond
 export const deleteMedicine = async (id) => {
   try {
     const result = await axios.delete(
-      `http://localhost:8080/api/medicine/${id}`
+        `http://localhost:8080/api/medicine/${id}`
     );
     return result;
   } catch (error) {
@@ -60,7 +75,7 @@ export const getMedicineList = async () => {
   try {
     const result = await axios.get(`http://localhost:8080/api/medicine/get-list`)
     return result.data;
-  }catch (error){
+  } catch (error) {
     return error;
   }
 }
