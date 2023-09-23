bfba8aebf2f6b6879ede3512d05a2aa4ae722152
thanhvo203 <vohaithanh21@gmail.com>
2023-09-23 19:07:27 +0700

    [ThanhVh][Supplier] commit hoan thanh tat ca chuc nang FE supplier ngayf 23/09 19:07

--- src/components/supplier/UpdateSupplierComponent.js
+++ src/components/supplier/UpdateSupplierComponent.js
@@ -17,11 +17,23 @@ function UpdateSupplierComponent() {
 
 
   const getSupplier = async () => {
-    const data = await getSupplierById(idSupplier);
-    setSupplier(data);
-
+     try {
+        const result = await getSupplierById(idSupplier);
+        setSupplier(result);
+     }catch (e) {
+        if(e.response.status === 400 || e.response.status === 404) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Lỗi kết nối',
+            text:'Không tìm thấy nhà cung cấp',
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+        navigate("/dashboard/supplier");
+     }
   }
-  const handleSubmit = async (value, setErrors) => {
+  const handleSubmit = async (value) => {
     try {
       await updateSupplierById(idSupplier,value);
       Swal.fire(
@@ -35,32 +47,12 @@ function UpdateSupplierComponent() {
       ).then(() => {navigate("/dashboard/supplier")});
     } catch (err) {
       console.log(err);
-      if (err.response.data) {
-        setErrors(err.response.data);
-      }
-      if (err.response.status === 406) {
-        console.log(err);
-        setErrors(err.response.data);
-      }
-      if(err.response.status === 404){
-        Swal.fire(
-          {
-            icon: 'warning',
-            title:'Không tìm thấy nhà cung cấp',
-            timer:2000,
-            showCancelButton:true,
-            showConfirmButton:false
-          }
-        ).then(() => {navigate("/dashboard/supplier")});
-      }
     }
   };
   useEffect(() => {
+    document.title = 'RetroCare - Chỉnh sửa nhà cung cấp'
     getSupplier()
   }, [idSupplier])
-  useEffect(() => {
-    document.title = 'RetroCare - Chỉnh sửa nhà cung cấp'
-},[])
 
   console.log(supplier.code);
   return (
@@ -68,8 +60,6 @@ function UpdateSupplierComponent() {
       {supplier.id &&
         <div id="ThanhVH">
           <meta charSet="UTF-8" />
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
           <div className="d-flex justify-content-center">
             <Formik
               initialValues={{
@@ -82,29 +72,29 @@ function UpdateSupplierComponent() {
               }}
               validationSchema={yup.object({
                 code: yup.string()
-                  .required("Không được để trống trường này")
-                  .min(3, "Mã nhà cung cấp tối thiểu 3 ký tự và tối đa 30 ký tự")
-                  .max(30, "Mã nhà cung cấp tối thiểu 3 ký tự và tối đa 30 ký tự")
-                  .matches(/^(?!.*[^A-Z])(?!.*\s)[A-Z]{3,30}$/, "Các ký tự được viết hoa,không có khoảng trắng,không có ký tự đặc biệt vd: NUTINE"),
+                  .required("Không được để trống trường này.")
+                  .min(3, "Mã nhà cung cấp tối thiểu 3 ký tự.")
+                  .max(30, "Mã nhà cung cấp tối đa 30 ký tự.")
+                  .matches(/^(?!.*[^A-Z])(?!.*\s)[A-Z]{3,30}$/, "Nhập sai định dạng vd: NUTINE."),
                 name: yup.string()
-                  .required("Không được để trống trường này")
-                  .min(3, "Tên nhà cung cấp tối thiểu 3 ký tự và tối đa 100 ký tự")
-                  .max(100, "Tên nhà cung cấp tối thiểu 3 ký tự và tối đa 100 ký tự")
-                  .matches(XRegExp('^\\p{Lu}\\p{Ll}*([\\s]\\p{Lu}\\p{Ll}*)*$'), "Vui lòng viết hoa chữ cái đầu của từng từ và có khoảng trắng giữa các từ, vd: Dược Phẩm Pharmacity"),
+                  .required("Không được để trống trường này.")
+                  .min(3, "Tên nhà cung cấp tối thiểu 3 ký tự.")
+                  .max(100, "Tên nhà cung cấp tối đa 100 ký tự.")
+                  .matches(XRegExp('^\\p{Lu}\\p{Ll}*([\\s]\\p{Lu}\\p{Ll}*)*$'),"Nhập sai định dạng vd: Dược Phẩm Pharmacity."),
                 email: yup.string()
-                  .required("Không được để trống trường này")
-                  .min(12, "Email tối thiểu 12 ký tự và tối đa 50 ký tự")
-                  .max(50, "Email tối thiểu 12 ký tự và tối đa 50 ký tự")
-                  .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Vui lòng nhập theo định dạng: xxx@xxx.xxx với x không phải là ký tự đặc biệt"),
+                  .required("Không được để trống trường này.")
+                  .min(12, "Email tối thiểu 12 ký tự.")
+                  .max(50, "Email tối đa 50 ký tự.")
+                  .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Nhập sai định dạng: xxx@xxx.xxx với x không phải là ký tự đặc biệt."),
                 address: yup.string()
-                  .required("Không được để trống trường này")
-                  .min(5, "Địa chỉ tối thiểu 5 ký tự và tối đa 150 ký tự")
-                  .max(150, "Địa chỉ tối thiểu 5 ký tự và tối đa 150 ký tự"),
+                  .required("Không được để trống trường này.")
+                  .min(5, "Địa chỉ tối thiểu 5 ký tự.")
+                  .max(150, "Địa chỉ tối đa 150 ký tự"),
                 phoneNumber: yup.string()
-                  .required("Không được để trống trường này")
-                  .min(10, "Số điện thoại vui lòng nhập 10 chữ số")
-                  .max(10, "Số điện thoại vui lòng nhập 10 chữ số")
-                  .matches(/^0[0-9]{9}$/, "Vui lòng nhập theo định dạng 0xxxxxxxxx với x là ký tự số")
+                  .required("Không được để trống trường này.")
+                  .min(10, "Số điện thoại vui lòng nhập 10 chữ số.")
+                  .max(10, "Số điện thoại vui lòng nhập 10 chữ số.")
+                  .matches(/^0[0-9]{9}$/, "Vui lòng nhập theo định dạng 0xxxxxxxxx với x là ký tự số.")
 
               })}
               onSubmit={(values, { setErrors }) => handleSubmit(values, setErrors)}
@@ -116,46 +106,54 @@ function UpdateSupplierComponent() {
                     <h2>Thay đổi thông tin nhà cung cấp</h2>
                   </legend>
                   <div className="row p-2">
-                    <div className="col-4 p-2">
+                  <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Mã nhà cung cấp <span style={{ color: 'red' }}>*</span> </label>
                     </div>
                     <div className="col-8">
                       <Field className="form-control mt-2 border border-dark" name='code' disabled type="text" />
-                      <ErrorMessage className="p-3 mb-2 text-danger" name='code' component='div'> </ErrorMessage>
+                      <ErrorMessage className="text-danger" name='code' component='div'> </ErrorMessage>
                     </div>
-                    <div className="col-4 p-2">
+                    <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Tên nhà cung cấp<span style={{ color: 'red' }}>*</span> </label>
                     </div>
                     <div className="col-8">
                       <Field className="form-control mt-2 border border-dark" name="name" type="text" />
-                      <ErrorMessage className="p-3 mb-2 text-danger" name="name" component='div' />
+                      <div style={{height: '40px'}}>
+                      <ErrorMessage className="text-danger" name="name" component='div' />
+                      </div>
                     </div>
-                    <div className="col-4 p-2">
+                    <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Số điện thoại <span style={{ color: 'red' }}>*</span></label>
                     </div>
                     <div className="col-8">
                       <Field className="form-control mt-2 border border-dark" type="text" name='phoneNumber' />
-                      <ErrorMessage className="p-3 mb-2 text-danger" name='phoneNumber' component={'div'} />
+                      <div style={{height: '40px'}}>
+                      <ErrorMessage className=" text-danger" name='phoneNumber' component={'div'} />
+                      </div>
                     </div>
-                    <div className="col-4 p-2">
+                    <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Địa chỉ </label>
                     </div>
                     <div className="col-8">
                       <Field className="form-control mt-2 border border-dark" type="text" name='address' />
-                      <ErrorMessage className="p-3 mb-2 text-danger" name='address' component={'div'} />
+                      <div style={{height: '30px'}}>
+                      <ErrorMessage className=" text-danger" name='address' component={'div'} />
+                      </div>
                     </div>
-                    <div className="col-4 p-2">
+                    <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Email <span style={{ color: 'red' }}>*</span></label>
                     </div>
                     <div className="col-8">
                       <Field className="form-control mt-2 border border-dark" type="email" name='email' />
-                      <ErrorMessage className="p-3 mb-2 text-danger" name='email' component={'div'} />
+                      <div style={{height: '40px'}}>
+                      <ErrorMessage className="text-danger" name='email' component={'div'} />
+                      </div>
                     </div>
-                    <div className="col-4 p-2">
+                    <div className="col-4 p-4" style={{marginTop:'-10px'}}>
                       <label>Ghi chú </label>
                     </div>
                     <div className="col-8">
-                      <Field className="form-control mt-2 border border-dark" as="textarea" name='note' />
+                      <Field className="form-control mt-2 border border-dark" as="textarea" name='note' style={{height:'100px'}} />
                     </div>
                     <div className="col-4 p-2 mt-3">
                       <div className="float-start">
@@ -163,7 +161,7 @@ function UpdateSupplierComponent() {
                       </div>
                     </div>
                     <div className="col-8 mt-3">
-                      <Link to={`/dashboard/supplier`} type="button" className="btn btn-outline-secondary  float-end mx-1 mt-2 shadow"><AiOutlineRollback className="fa-solid fa-rotate-left" />Trở về</Link>
+                      <Link to={`/dashboard/supplier`} type="button" className="btn btn-outline-secondary  float-end mx-1 mt-2 shadow"><AiOutlineRollback />Trở về</Link>
                       <button className="btn btn-outline-primary float-end mx-1 mt-2 shadow" type="submit">
                       <FiEdit className="mx-1" /> Hoàn thành
                       </button>
