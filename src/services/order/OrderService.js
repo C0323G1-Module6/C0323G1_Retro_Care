ea266c67294036abf383f5f1e14b6ff7a24f8f0e
Tran Thanh Son <sonva2002@gmail.com>
2023-09-24 09:19:07 +0700

    SonTT update employee

--- src/services/order/OrderService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function createOrder(appUserId, loyaltyPoint, totalPrice) {
-  const res = await axios.post(
-    `http://localhost:8080/api/orders/create?appUserId=${appUserId}
-    &loyaltyPoint=${loyaltyPoint}&totalPrice=${totalPrice}`
-  );
-}
--- src/services/prescription/patient.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getAllPatient =async () => {
-    const res = await axios.get("http://localhost:8080/patient");
-    return res.data;
-}
