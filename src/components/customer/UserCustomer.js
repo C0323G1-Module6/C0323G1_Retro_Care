f743f0f2d525f8199e60db4258e8bc28c19ac98d
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-09-25 21:05:57 +0700

    HuyDH_Invoice update FE invoiceList 5nd day 25/09/2023

--- src/components/customer/UserCustomer.js
+++ src/components/customer/UserCustomer.js
@@ -130,160 +130,160 @@ const UserCustomer = () => {
             >
                 {({ isValid,dirty }) => (
                     <div   className="row shadow mx-auto image-with-body"
-                       style={{width: "80%", marginTop: "2%", borderRadius: 20, marginLeft: "2%"}}>
-                <div className="col-5 image-with-text">
-                    <div style={{marginTop: "8%"}}>
-                        {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Bạn đã cập nhật ?</h5>*/}
-                        {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
-                        {/*    Để xem các sản phẩm, giá cả và ưu đãi mới nhất, bạn cần phải đăng ký.*/}
-                        {/*    Vui lòng nhập thông tin của bạn để chúng tôi có thể liên hệ với bạn và*/}
-                        {/*    gửi cho bạn liên kết đến tất cả các sản phẩm.*/}
-                        {/*</p>*/}
-                    </div>
-                    <div className=" mt-5">
-                        {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Bạn đã cập nhật trước khi ?</h5>*/}
-                        {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
-                        {/*    Nếu bạn đã đăng ký trước đó nhưng quên hoặc không tìm thấy liên kết*/}
-                        {/*    chúng tôi đã gửi cho bạn, vui lòng gọi cho chúng tôi hoặc gửi email cho*/}
-                        {/*    chúng tôi. Bạn có thể đăng ký lại nếu muốn, nhưng không cần thiết nếu*/}
-                        {/*    bạn đã đăng ký rồi.*/}
-                        {/*</p>*/}
-                    </div>
-                    <div className="mt-5">
-                        {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Làm sao để cập nhật thông tin ?</h5>*/}
-                        {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
-                        {/*    Vui lòng điền đầy đủ thông tin để được cập nhật, sau đó hãy bấm nút cập*/}
-                        {/*    nhật để kết thúc thao tác.*/}
-                        {/*</p>*/}
-                    </div>
-                </div>
-                        <Form className="col-7 px-3 pt-3" style={{borderRadius: 20}}>
-                        <h2 style={{color: "#6C757D", marginLeft: "2rem"}}>
-                            Hồ sơ thông tin cá nhân
-                        </h2>
-                        <hr/>
-                        <div className="mx-auto pt-2 pb-3" style={{width: "90%"}}>
-                            <div className="mb-3">
-                                <label htmlFor="name" className="form-label">
-                                    Họ và tên <small style={{color: "red"}}>*</small>
-                                </label>
-                                <Field
-                                    type="text"
-                                    className="form-control"
-                                    id="name"
-                                    placeholder=" Chưa có thông tin vd: Nguyễn Văn An"
-                                    name="name"
-                                />
-                                <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
-                                    <ErrorMessage
-                                        className=" text-danger"
-                                        name="name"
-                                        component="small"
-                                    />
-                                </div>
+                           style={{width: "80%", marginTop: "2%", borderRadius: 20, marginLeft: "2%"}}>
+                        <div className="col-5 image-with-text">
+                            <div style={{marginTop: "8%"}}>
+                                {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Bạn đã cập nhật ?</h5>*/}
+                                {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
+                                {/*    Để xem các sản phẩm, giá cả và ưu đãi mới nhất, bạn cần phải đăng ký.*/}
+                                {/*    Vui lòng nhập thông tin của bạn để chúng tôi có thể liên hệ với bạn và*/}
+                                {/*    gửi cho bạn liên kết đến tất cả các sản phẩm.*/}
+                                {/*</p>*/}
                             </div>
-                            <div className="mb-3">
-                                <label htmlFor="email" className="form-label">
-                                    Địa chỉ email <small style={{color: "red"}}>*</small>
-                                </label>
-                                <Field
-                                    type="email"
-                                    className="form-control"
-                                    id="email"
-                                    placeholder="name@example.com"
-                                    name="email"
-                                />
-                                <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
-                                    <ErrorMessage
-                                        className=" text-danger"
-                                        name="email"
-                                        component="small"
-                                    />
-                                </div>
+                            <div className=" mt-5">
+                                {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Bạn đã cập nhật trước khi ?</h5>*/}
+                                {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
+                                {/*    Nếu bạn đã đăng ký trước đó nhưng quên hoặc không tìm thấy liên kết*/}
+                                {/*    chúng tôi đã gửi cho bạn, vui lòng gọi cho chúng tôi hoặc gửi email cho*/}
+                                {/*    chúng tôi. Bạn có thể đăng ký lại nếu muốn, nhưng không cần thiết nếu*/}
+                                {/*    bạn đã đăng ký rồi.*/}
+                                {/*</p>*/}
                             </div>
-                            <div className="mb-3">
-                                <label htmlFor="phoneNumber" className="form-label">
-                                    Số điện thoại <small style={{color: "red"}}>*</small>
-                                </label>
-                                <Field
-                                    type="text"
-                                    className="form-control"
-                                    id="phoneNumber"
-                                    placeholder="VD: 0339779751"
-                                    name="phoneNumber"
-                                />
-                                <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
-                                    <ErrorMessage
-                                        className=" text-danger"
-                                        name="phoneNumber"
-                                        component="small"
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="birthday" className="form-label">
-                                    Ngày sinh <small style={{color: "red"}}>*</small>
-                                </label>
-                                <Field
-                                    type="date"
-                                    className="form-control"
-                                    id="birthday"
-                                    name="birthday"
-                                />
-                                <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
-                                    <ErrorMessage
-                                        className=" text-danger"
-                                        name="birthday"
-                                        component="small"
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="address" className="form-label">
-                                    Địa chỉ <small style={{color: "red"}}>*</small>
-                                </label>
-                                <Field
-                                    as="textarea"
-                                    className="form-control"
-                                    id="address"
-                                    name="address"
-                                    placeholder="Vd :120 tên đường, quận/huyện , tỉnh/thành"
-                                    defaultValue={""}
-                                />
-                                <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
-                                    <ErrorMessage
-                                        className=" text-danger"
-                                        name ="address"
-                                        component ="small"
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-3 row">
-                                <div className="col-7" style={{float: "start"}}>
-                                    <small className="text-danger">(*)</small> Thông tin cần thiết để mua
-                                    hàng
-                                </div>
-                                <div className="col-3">
-                                    { isValid && dirty && (
-                                        <button
-                                            className="btn btn-success float-end mx-1 mt-2 shadow"
-                                            type="submit"
-                                        >Cập nhật
-                                        </button>)}
-                                </div>
-                                <div className="col-2">
-                                    <button
-                                        type="button"
-                                        onClick={comeBackPagePrev}
-                                        className="btn btn-outline-secondary float-end mx-1 mt-2 shadow"
-                                    >
-                                        Trở về
-                                    </button>
-                                </div>
+                            <div className="mt-5">
+                                {/*<h5 style={{fontWeight: "bold", color: "whitesmoke"}}>Làm sao để cập nhật thông tin ?</h5>*/}
+                                {/*<p style={{color: "whitesmoke", fontWeight: "bold"}}>*/}
+                                {/*    Vui lòng điền đầy đủ thông tin để được cập nhật, sau đó hãy bấm nút cập*/}
+                                {/*    nhật để kết thúc thao tác.*/}
+                                {/*</p>*/}
                             </div>
                         </div>
+                        <Form className="col-7 px-3 pt-3" style={{borderRadius: 20}}>
+                            <h2 style={{color: "#6C757D", marginLeft: "2rem"}}>
+                                Hồ sơ thông tin cá nhân
+                            </h2>
+                            <hr/>
+                            <div className="mx-auto pt-2 pb-3" style={{width: "90%"}}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">
+                                        Họ và tên <small style={{color: "red"}}>*</small>
+                                    </label>
+                                    <Field
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        placeholder=" Chưa có thông tin vd: Nguyễn Văn An"
+                                        name="name"
+                                    />
+                                    <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
+                                        <ErrorMessage
+                                            className=" text-danger"
+                                            name="name"
+                                            component="small"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">
+                                        Địa chỉ email <small style={{color: "red"}}>*</small>
+                                    </label>
+                                    <Field
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="name@example.com"
+                                        name="email"
+                                    />
+                                    <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
+                                        <ErrorMessage
+                                            className=" text-danger"
+                                            name="email"
+                                            component="small"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="phoneNumber" className="form-label">
+                                        Số điện thoại <small style={{color: "red"}}>*</small>
+                                    </label>
+                                    <Field
+                                        type="text"
+                                        className="form-control"
+                                        id="phoneNumber"
+                                        placeholder="VD: 0339779751"
+                                        name="phoneNumber"
+                                    />
+                                    <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
+                                        <ErrorMessage
+                                            className=" text-danger"
+                                            name="phoneNumber"
+                                            component="small"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="birthday" className="form-label">
+                                        Ngày sinh <small style={{color: "red"}}>*</small>
+                                    </label>
+                                    <Field
+                                        type="date"
+                                        className="form-control"
+                                        id="birthday"
+                                        name="birthday"
+                                    />
+                                    <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
+                                        <ErrorMessage
+                                            className=" text-danger"
+                                            name="birthday"
+                                            component="small"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="address" className="form-label">
+                                        Địa chỉ <small style={{color: "red"}}>*</small>
+                                    </label>
+                                    <Field
+                                        as="textarea"
+                                        className="form-control"
+                                        id="address"
+                                        name="address"
+                                        placeholder="Vd :120 tên đường, quận/huyện , tỉnh/thành"
+                                        defaultValue={""}
+                                    />
+                                    <div style={{height: "0.6rem", marginBottom: "0.6rem"}}>
+                                        <ErrorMessage
+                                            className=" text-danger"
+                                            name ="address"
+                                            component ="small"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-3 d-flex justify-content-between align-items-center">
+                                    <div  style={{float: "start"}}>
+                                        <small className="text-danger">(*)</small> Thông tin cần thiết để mua
+                                        hàng
+                                    </div>
+                                    <div >
+                                        { isValid && dirty && (
+                                            <button
+                                                className="btn btn-success float-end mx-1 mt-2 shadow"
+                                                type="submit"
+                                            >Cập nhật
+                                            </button>)}
+                                    </div>
+                                    <div >
+                                        <button
+                                            type="button"
+                                            onClick={comeBackPagePrev}
+                                            className="btn btn-outline-secondary float-end mx-1 mt-2 shadow"
+                                        >
+                                            Trở về
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </Form>
-                </div>)}</Formik>
+                    </div>)}</Formik>
 
         </>
     );
