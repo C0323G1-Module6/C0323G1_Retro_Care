682e06a08fafd72fce2ac8d35b63230a0eadcead
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-09-25 19:44:15 +0700

    [user][NhatNHH] restore app.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,8 @@ import ReturnVNPay from "./components/order/ReturnVNPay";
 import ListInvoiceOrder from "./components/order/ListInvoiceOrder";
 import EditInvoice from "./components/invoice/EditInvoice";
 import CreateInvoice from "./components/invoice/CreateInvoice";
-// import Error403 from "./components/user/Error403";
+import MedicinesWithKind from "./components/home/MedicinesWithKind";
+import Error403 from "./components/user/Error403";
 function App() {
   axiosClient();
   return (
@@ -54,12 +55,16 @@ function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/home/search/:keyword" element={<SearchPage />} />
         <Route path="/home/search/" element={<SearchPage />} />
+        <Route
+          path="/home/list-medicines/:type"
+          element={<MedicinesWithKind />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/details/:id" element={<Details />}></Route>
         <Route path="/user-infor/:id" element={<UserCustomer />} />
         <Route path="/cart" element={<Cart />} />
-        {/* <Route path="/403" element={<Error403 />} /> */}
+        <Route path="/403" element={<Error403 />} />
         <Route
           element={
             <Authentication
